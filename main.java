9174f8ca066b2ab4c2109b71ab270ed1f843a744
nurpoyraz15 <112077894+nurpoyraz15@users.noreply.github.com>
2022-09-21 16:42:09 +0300

    Add files via upload

--- main.java
+++ main.java
@@ -1,62 +1,63 @@
-package sartblok;
+package hellojav;
 
 public class main {
-
 	public static void main(String[] args) {
-		// Şart Blokları
-		//1.if-else blokları
-		int sayi1 = 36;
-	    int sayi2 = 33;
-	    int sayi3 =35;
-	    int enKucuk = sayi3;
-	    if (sayi2< enKucuk) {
-	    enKucuk= sayi2;
-	    }
-        if (sayi1< enKucuk) {
-        enKucuk = sayi1;	
-        }
-        System.out.println("En Küçük:"+ enKucuk);
-        
-        System.out.println("--------");
-        // 2.Switch Blokları
-        char city= 'İ';
-        switch (city) {
-        case 'K':
-        System.out.println("Konya'nın yemekleri güzeldir.");
-        break;
-        case 'A':
-        	System.out.println("Ankara'nın yemekleri güzeldir.");
-        	break;
-        case 'İ':
-        	System.out.println("İstanbul'un yemekleri güzeldir.");
-        	break;
-        case 'T':
-        	System.out.println("Trabzon'un yemekleri güzeldir.");
-        	break;
-        default:
-        	System.out.println("Diğer şehirlerin yemekleri de güzeldir.");
-        	{
-        		
-        	}
-        	
-       
-        	
-        
-        
-        
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-	    }
-        
+		System.out.println ("Hello World!");
+		System.out.println ("--------");
+		
+		
+		int sinifMevcudu = 18;
+		System.out.println (sinifMevcudu);
+		System.out.println ("Sınıf sayısı:"+ sinifMevcudu );
+		// Sınıf sayısı ifadesini değiştirmek için tek tek değiştirme yerine değişken atayabiliriz.
+		String mesaj = "Sınıf toplamı:";
+		System.out.println (mesaj + sinifMevcudu );
+        System.out.println ("--------");
+		
+		
+		// Veri tipleri:
+		//1. integer (tam sayı)
+		int number1 = 15;
+		System.out.println (number1);
+		//2.byte (tam sayı)
+		byte number2 = 127;
+		System.out.println (number2);
+     	//3. short (tam sayı)
+		short number3 = 1562;
+		System.out.println (number3);
+		//4.long (tam sayı)
+		long number4 = 46789;
+		System.out.println (number4);
+		//yukarıdaki veri tiplerinin yalnız iki farkı bulunmaktadır. birinci olarak hafızada tuttukları byte, ikinci olarak yazılabilecek sayı sınırları birbirinden farklıdır.
+		//5.double (ondalıklı sayı)
+		double number5 = 426.23;
+		System.out.println (number5);
+		//6. float (ondalıklı sayı)
+		float number6 = (float) 12.6;
+	System.out.println (number6);
+		// double ve float farkı: byte'ları ve yazılacak sayının sınırları.
+		//7.char (tek karakter)
+		char karakter = 'N';
+		System.out.println (karakter);
+		//8. boolean (true/false)
+		boolean dogruMU= true;
+		System.out.println (dogruMU);
+		// en çok integer ve double tercih edilmektedir.
+		
+		System.out.println ("--------");
+		
+		
+		
+			
+			
+			
+			
+				
+		
+		
+		
+		
+
+	}
+
 }
-	
-	
